dd055a5cc42b674b3184e8cea5bd4fac1dcfe449
Dylan De Forrest <deforred@oregonstate.edu>
2026-03-12 20:45:42 -0700

    implemented water reservoir test code

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -2,4 +2,10 @@ require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
 
+  it 'has reservoir' do 
+    reservoir = WaterReservoir.new
+    dispenser = WaterDispenser.new(reservoir)
+    expect(dispenser.reservoir).to eq(reservoir)
+  end 
+
 end
--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -2,4 +2,31 @@ require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
 
+  it 'has capacity' do 
+    reservoir = WaterReservoir.new()
+    expect(reservoir.capacity).to eq(10)
+  end 
+
+  it 'starts empty' do 
+    reservoir = WaterReservoir.new()
+    expect(reservoir).to be_empty
+  end
+
+  it 'can be initiated with water' do 
+    reservoir = WaterReservoir.new(10, 5)
+    expect(reservoir.current_water_volume).to eq(5)
+  end
+
+  it 'can be filled to capacity' do 
+    reservoir = WaterReservoir.new
+    reservoir.fill
+    expect(reservoir.current_water_volume).to eq(reservoir.capacity)
+  end
+
+  it 'can be drained' do 
+    reservoir = WaterReservoir.new(10,10)
+    reservoir.drain(3)
+    expect(reservoir.current_water_volume).to eq(7)
+  end
+
 end
