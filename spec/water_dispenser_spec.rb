0d0f89bcc8b4320b77872f9a6b75dec8b9e892e8
Dylan De Forrest <deforred@oregonstate.edu>
2026-03-12 20:52:34 -0700

    implemented water dispenser test code

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -8,4 +8,12 @@ describe 'A water dispenser' do
     expect(dispenser.reservoir).to eq(reservoir)
   end 
 
+  it 'drains in to a vessel' do 
+    reservoir = WaterReservoir.new(10, 10)
+    dispenser = WaterDispenser.new(reservoir)
+    vessel = Vessel.new('cup', 3)
+    dispenser.dispense(vessel)
+    expect(reservoir.current_water_volume).to eq(7)
+  end
+
 end
